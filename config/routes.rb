54ccc2fcc68605d4368d3096bcab732ed44568e5
dring1 <devon.ring@gmail.com>
2013-10-01 23:12:38 -0400

    added basic website

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 OnesZerosProject::Application.routes.draw do
+  
+  root "static_pages#home"
+  get '/about', to:"static_pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
