69f2de7132aee7f2e1f415f4f1df5f6e701d1517
dring1 <devon.ring@gmail.com>
2014-01-31 10:07:29 -0500

    added beta note

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,23 @@ class ArticlesController < ApplicationController
 
 	  before_filter :authenticate_user!, except: [:index, :show]
 
+
+	  def new
+	  end
+
+	  def create
+	  end
+
+	  def show
+	  	
+	  end
+
+	  def index
+	  	
+	  end
+
+	  def destroy
+	  	
+	  end
+
 end
